661b7aa8310c31d27f0301f1f3e2dd8bd1a39ca7
Jan Hamal Dvořák <mordae@anilinux.org>
2022-02-09 11:05:14 +0100

    Initial import
    Signed-off-by: Jan Hamal Dvořák <mordae@anilinux.org>

---
+++ README.md
@@ -0,0 +1,14 @@
+# KeyValue
+
+**Class and an operator for key-value tuple composition.**
+
+Example:
+
+```haskell
+import Data.KeyValue
+
+animalNames :: [(String, String)]
+animalNames = [ "dog" := "Ajax"
+              , "cat" := "Micka"
+              ]
+```
---
+++ lib/Data/KeyValue.hs
@@ -0,0 +1,28 @@
+-- |
+-- Module      :  Data.KeyValue
+-- Copyright   :  Jan Hamal Dvořák
+-- License     :  MIT
+--
+-- Maintainer  :  mordae@anilinux.org
+-- Stability   :  unstable
+-- Portability :  non-portable (ghc)
+--
+
+module Data.KeyValue
+  ( KeyValue(..)
+  )
+where
+  -- |
+  -- A key-value pair.
+  --
+  class KeyValue kv k v where
+    (.=) :: k -> v -> kv
+    infixr 8 .=
+
+
+  instance KeyValue (a, b) a b where
+    k .= v = (k, v)
+    {-# INLINE (.=) #-}
+
+
+-- vim:set ft=haskell sw=2 ts=2 et:
